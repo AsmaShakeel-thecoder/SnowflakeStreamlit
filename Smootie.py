37909765888bb670345424229beb5f588a51a3de
Asma Shakeel <97561088+AsmaShakeel-thecoder@users.noreply.github.com>
2024-08-11 03:34:15 +0500

    Update Smootie.py

--- Smootie.py
+++ Smootie.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.functions import col
+#from snowflake.snowpark.functions import col
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize your Smoothies :cup_with_straw:")
@@ -8,6 +8,7 @@ st.write(
     """Choose the fruits you want in your custom smoothie!
     """
 )
+'''
 name_on_order = st.text_input('Name on Smoothie: ')
 st.write:('The name on your Smoothie will be: ', name_on_order)
 
@@ -31,3 +32,4 @@ if ingredients_list:
         st.success("Your Smoothie is ordered", icon="🙌")
 
         
+'''
